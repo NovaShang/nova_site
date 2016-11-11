f74fca5ed463fb8b5439a6f6fa01900e07dceb72
styleshang <styleshang@gmail.com>
2016-11-11 20:37:52 +0800

    delete Web3D and nova_calendar app

--- nova_calendar/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from nova_calendar.models import *
-
-admin.site.register(Task)
--- nova_calendar/serializers.py
+++
@@ -1,9 +0,0 @@
-from nova_calendar.models import *
-from rest_framework import serializers
-
-class TaskSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model=Task
-        fields=('name','done','description','deadline','create_time','finish_time')
-
-
